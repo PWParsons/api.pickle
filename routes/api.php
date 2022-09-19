4bba27b3d465fc636b44ebe61fab69672f4c0b32
zpwparsons <peterp@zero2one.co.za>
2022-09-19 19:50:22 +0200

    feat: add route for getting a listing of collections

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,13 @@
 
 use App\Actions\Collections\CreateCollection;
 use App\Actions\Collections\DeleteCollection;
+use App\Actions\Collections\GetAllCollections;
 use App\Actions\Collections\GetCollection;
 use App\Actions\Collections\UpdateCollection;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('collections')->name('collections.')->group(function () {
+    Route::get('/', GetAllCollections::class)->name('index');
     Route::post('/', CreateCollection::class)->name('store');
     Route::get('{collection:uuid}', GetCollection::class)->name('show');
     Route::patch('{collection:uuid}', UpdateCollection::class)->name('update');
